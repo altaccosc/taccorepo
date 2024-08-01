28c603320f271118ae4bf5c7772a5229f6a08973
altaccosc <97206249+altaccosc@users.noreply.github.com>
2024-08-01 03:40:28 +0200

    Push finished repo to GitHub

---
+++ .github/scripts/commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+git status
+if [ -n "$(git status --porcelain)" ]; then
+	git add .
+	git commit -m "Update extensions repo"
+	git push
+else
+	echo "No changes to commit"
+fi
